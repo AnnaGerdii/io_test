6c9b749cc828d1cd79eef90859e838518259704b
Anna Gerdii <annagerdiy@gmail.com>
2020-01-26 22:09:11 +0200

    Updates

---
+++ precache-manifest.8de078d75471af87b232f9ba54d45172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95255367447e53cb2a364884f19ee031",
+    "url": "/io_test/index.html"
+  },
+  {
+    "revision": "31a12400aec13ec0f188",
+    "url": "/io_test/static/css/main.4752e3fc.chunk.css"
+  },
+  {
+    "revision": "8c273a17c4b7200a9d71",
+    "url": "/io_test/static/js/2.90450834.chunk.js"
+  },
+  {
+    "revision": "31a12400aec13ec0f188",
+    "url": "/io_test/static/js/main.b0c148eb.chunk.js"
+  },
+  {
+    "revision": "45a8dd488996bcf5f510",
+    "url": "/io_test/static/js/runtime~main.592d42a9.js"
+  },
+  {
+    "revision": "30820dcaea0e6a2b7e9b178c40eb3246",
+    "url": "/io_test/static/media/bg-decor.30820dca.svg"
+  }
+]);
